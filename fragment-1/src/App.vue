d41755ac4bf3da8bce440bb33eb5f6c6c34aa3b8
Pongsatorn Nitithammawoot <biig_pongsatorn@hotmail.com>
2018-09-26 14:19:05 +0700

    add fragments

---
+++ fragment-1/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="fragment-1">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your fragment-1 App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'fragment-1',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#fragment-1 {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
