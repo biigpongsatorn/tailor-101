2e4a833a42a1ee37a06e38e21e301a628de85ddc
Pongsatorn Nitithammawoot <biig_pongsatorn@hotmail.com>
2018-09-26 14:24:56 +0700

    build fragment 2

--- fragment-2/src/App.vue
+++ fragment-2/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="fragment-2">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Fragment-2 App"/>
   </div>
@@ -17,7 +17,7 @@ export default {
 </script>
 
 <style>
-#app {
+#fragment-2 {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
